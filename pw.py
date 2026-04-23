2e8956b22120d01d8c0d36ab11eec1774d12a178
ty1005 <tzuyin1005@gmail.com>
2026-04-23 18:21:55 +0800

    Add files via upload

--- pw.py
+++ pw.py
@@ -1,12 +1,13 @@
 password = 'a123456'
 i = 3 #3次機會
-while True:
+while i > 0:
     question = input('請輸入密碼：')
     if question == password:
         print('登入成功')
         break #逃出while
     else:
-    	i = i - 1
-        print('密碼錯誤！還有', i,'次機會')
-        if i == 0:
-	    break
+        i = i - 1
+        if i > 0:
+            print('密碼錯誤！還有', i,'次機會')
+        else:
+            print('密碼錯誤！已鎖定')
